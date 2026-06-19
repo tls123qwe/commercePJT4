14c24737076408915eeab9c29d00c639dbad8871
tls123qwe <tls123qwe@gmail.com>
2026-06-19 14:24:21 +0900

    Step4까지 완료

---
+++ src/commerce/Customer.java
@@ -0,0 +1,13 @@
+package commerce;
+
+public class Customer {
+    private String customerName;
+    private String email;
+    private String grade;
+
+    public Customer(String customerName, String email, String grade) {
+        this.customerName = customerName;
+        this.email = email;
+        this.grade = grade;
+    }
+}
